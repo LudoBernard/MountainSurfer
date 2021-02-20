9c519a6d488578faa4bcf377b8916a5e541a28f2
Victor gonnet <vicy@live.Fr>
2021-02-20 16:42:36 +0100

    on est là

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    private Transform player_;
+    private Text scoreText_;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText_.text = (player_.position.z * 100).ToString("0");
+    }
+}
